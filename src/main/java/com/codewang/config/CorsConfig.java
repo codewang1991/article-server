eb26cb400b5bac838de87b0f9b29c0c0dd38a3fa
codewang <1076953055@qq.com>
2019-10-23 20:37:31 +0800

    配置端口和及允许跨域访问

---
+++ src/main/java/com/codewang/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.codewang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+    }
+}
